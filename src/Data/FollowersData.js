6243f0ab302c157b03ca167b0daf90f76fd1c27b
MoAlhaddad <mohamadalhaddad25@gmail.com>
2022-06-01 20:06:34 -0700

    Initial Commit

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.jpg";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.jpg";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+    {name: "Ronaldo Cris", username: "CristianoRonaldo", img: img1},
+    { name: "Kanye West", username: "IneedMyWifeback", img: img2},
+    {name: "Lebron James", username: "lefraud", img: img3},
+    { name: "Sasuke Uchica", username: "antisocial", img: img4}
+];
